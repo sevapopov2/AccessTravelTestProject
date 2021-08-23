f61bc50cb3c2789e38de9bfc29f9f46764f275c3
Vsevolod Popov <sevapopov13@gmail.com>
2021-08-23 14:26:41 +0300

    Guides, Tours and Things to do xpaths are added.

--- src/main/java/Pages/BasePage.java
+++ src/main/java/Pages/BasePage.java
@@ -1,18 +1,18 @@
 package Pages;
 
-import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class BasePage {
     protected static WebDriver webDriver;
     protected static WebDriverWait wait;
 
-    public void setWebdriver(WebDriver webDriver) {
+    public void setWebDriver(WebDriver webDriver) {
         this.webDriver = webDriver;
         wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
     }
@@ -31,8 +31,7 @@ public class BasePage {
         try {
             findElementByXpath(xpath);
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return false;
         }
     }
--- src/main/java/Pages/MainPage.java
+++ src/main/java/Pages/MainPage.java
@@ -2,10 +2,12 @@ package Pages;
 
 import Consts.Consts;
 
-public class MainPage extends BasePage{
+public class MainPage extends BasePage {
     private static final String LOGO = "//*[@alt='Alt']";
-    private static final String HOTELS_LINK = "//a[text() = ' HOTELS']";
-
+    private static final String HOTELS_LINK = "//a[text() = ' Hotels']";
+    private static final String GUIDES_LINK = "//a[contains(text(), 'Guides')]";
+    private static final String TOURS_LINK = "//a[contains(text(), 'Tours')]";
+    private static final String THINGS_TO_DO_LINK = "//a[contains(text(), 'Things to do')]"
     public void navigateToMainPage() {
         webDriver.get(Consts.MAIN_URL);
     }
