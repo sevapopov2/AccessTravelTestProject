4de627254ca11a2ffbb8003863ba8403fe1b1d10
Vsevolod Popov <sevapopov13@gmail.com>
2021-08-24 13:40:01 +0300

    Added xpath for destinations list and piter option.

--- src/main/java/Pages/HotelsPage.java
+++ src/main/java/Pages/HotelsPage.java
@@ -2,8 +2,16 @@ package Pages;
 
 public class HotelsPage extends BasePage{
     private static final String HOTELS_HEADING = "//h3[@class='sub-heading']";
+    private static final String DESTINATIONS_LIST = "//select[@id='Filter_DestinationId']";
+    private static final String PITER_OPTION = "//select[@id='Filter_DestinationId']/option[5]";
     public boolean headingIsVisible() {
         boolean isVisible = elementExists(HOTELS_HEADING);
         return isVisible;
+    }
+    public void destinationsListSelection() {
+        clickElementByXpath(DESTINATIONS_LIST);
+    }
+    public void piterOptionSelection() {
+        clickElementByXpath(PITER_OPTION);
     }
 }
