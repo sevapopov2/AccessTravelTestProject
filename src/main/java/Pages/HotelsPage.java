61573197a394eaebeff74dffadc6ad4ccfcf52f7
Vsevolod Popov <sevapopov13@gmail.com>
2021-09-11 11:59:26 +0300

    Checkin and checkout date, adults and children count xpathes are added.

--- src/main/java/Pages/HotelsPage.java
+++ src/main/java/Pages/HotelsPage.java
@@ -4,17 +4,22 @@ public class HotelsPage extends BasePage{
     private static final String HOTELS_HEADING = "//h3[@class='sub-heading']";
     private static final String DESTINATIONS_LIST = "//select[@id='Filter_DestinationId']";
     private static final String PITER_OPTION = "//select[@id='Filter_DestinationId']/option[5]";
+    private static final String CHECKIN_DATE = "//input[@name='Filter.CheckIn']";
+    private static final String CHECKOUT_DATE = "//input[@name='Filter.CheckOut']";
+    private static final String ADULTS_COUNT_EDITOR = "//input[@id='Filter_AdultNum";
+    private static final String CHILDREN_COUNT_EDITOR = "//input[@id='Filter_ChildrenNum]";
+    private static final String SEARCH_BUTTON = "//button[@type='submit']";
     public boolean headingIsVisible() {
         boolean isVisible = elementExists(HOTELS_HEADING);
         return isVisible;
-    }
-    public boolean destinationsListFind() {
-        return elementExists(DESTINATIONS_LIST);
     }
     public void destinationsListSelection() {
         clickElementByXpath(DESTINATIONS_LIST);
     }
     public String piterOptionFind() {
         return findElementByXpath(PITER_OPTION).getText();
+    }
+    public void checkinDateChanging(String checkinDate) {
+        sendKeysToElementByXpath(CHECKIN_DATE, checkinDate);
     }
 }
