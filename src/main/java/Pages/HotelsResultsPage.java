e7cb3061a4220ed186f1f6ae60d55d1f192daab3
Vsevolod Popov <sevapopov13@gmail.com>
2021-09-17 18:23:10 +0300

    Hotels results page is added.

---
+++ src/main/java/Pages/HotelsResultsPage.java
@@ -0,0 +1,8 @@
+package Pages;
+
+public class HotelsResultsPage extends BasePage{
+    private static final String VIEW_HOTEL_LINK = "//a[contains(text(), 'View Hotel']";
+    public boolean resultsAreAvailable() {
+        return elementExists(VIEW_HOTEL_LINK);
+    }
+}
