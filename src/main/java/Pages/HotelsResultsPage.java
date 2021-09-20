fa33c184d6892444db147b8afb663a674c952d4f
Vsevolod Popov <sevapopov13@gmail.com>
2021-09-20 15:31:35 +0300

    Search button xpath was fixed.

--- src/main/java/Pages/HotelsResultsPage.java
+++ src/main/java/Pages/HotelsResultsPage.java
@@ -1,7 +1,7 @@
 package Pages;
 
 public class HotelsResultsPage extends BasePage{
-    private static final String VIEW_HOTEL_LINK = "//a[contains(text(), 'View Hotel']";
+    private static final String VIEW_HOTEL_LINK = "//a[contains(text(), 'View Hotel')]";
     public boolean resultsAreAvailable() {
         return elementExists(VIEW_HOTEL_LINK);
     }
