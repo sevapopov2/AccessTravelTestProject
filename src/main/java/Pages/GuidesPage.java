09a0871a0082c301eb73f3d7d3d22057535ca02e
Vsevolod Popov <sevapopov13@gmail.com>
2021-10-19 14:59:15 +0300

    Guides type selection parameterized test is added.

--- src/main/java/Pages/GuidesPage.java
+++ src/main/java/Pages/GuidesPage.java
@@ -8,6 +8,12 @@ public class GuidesPage extends BasePage {
     private static final String COUNTRY_RUSSIA_OPTION = "//select[@id='Filter_CountryId']/option[9]";
     private static final String DESTINATION_SELECTION_LIST = "//select[@id='Filter_DestinationId']";
     private static final String PITER_OPTION = "//select[@id='Filter_DestinationId']/option[3]";
+    //    guide types
+    private static final String GUIDE_TYPE_LIST = "//select[@id='Filter_GuideType']";
+    private static final String ACCOMPANY_GUIDE = "//select[@id='Filter_GuideType']/option[2]";
+    private static final String VOLUNTEER_GUIDE = "//select[@id='Filter_GuideType']/option[3]";
+    private static final String ONLINE_ADVISOR_GUIDE = "//select[@id='Filter_GuideType']/option[4]";
+    private static final String PROFESSIONAL_GUIDE = "//select[@id='Filter_GuideType']/option[5]";
 
     public boolean isHeadingVisible() {
         return elementExists(GUIDES_HEADING);
@@ -16,9 +22,21 @@ public class GuidesPage extends BasePage {
     public boolean countrySelectorVisible() {
         return elementExists(COUNTRY_SELECTION_LIST);
     }
+
     public void russiaPiterSelection() {
         sendKeypressesByXpath(COUNTRY_SELECTION_LIST, Keys.ENTER);
         clickElementByXpath(COUNTRY_RUSSIA_OPTION);
         clickElementByXpath(PITER_OPTION);
+    }
+
+    public String guideTypeSelection(String name) {
+        sendKeypressesByXpath(GUIDE_TYPE_LIST, Keys.ENTER);
+        clickElementByXpath(name);
+        return findElementByXpath(name).getText();
+    }
+
+    public String accompanyguideSelection() {
+        clickElementByXpath(ACCOMPANY_GUIDE);
+        return findElementByXpath(ACCOMPANY_GUIDE).getText();
     }
 }
--- src/test/java/GuidesPageTest.java
+++ src/test/java/GuidesPageTest.java
@@ -4,12 +4,18 @@ import Utilities.UseCaseBase;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GuidesPageTest extends UseCaseBase {
     private static MainPage mainPage;
     private static GuidesPage guidesPage;
+    private static final String ACCOMPANY_GUIDE = "//select[@id='Filter_GuideType']/option[2]";
+    private static final String VOLUNTEER_GUIDE = "//select[@id='Filter_GuideType']/option[3]";
+    private static final String ONLINE_ADVISOR_GUIDE = "//select[@id='Filter_GuideType']/option[4]";
+    private static final String PROFESSIONAL_GUIDE = "//select[@id='Filter_GuideType']/option[5]";
 
     @BeforeAll
     public static void classSetup() {
@@ -28,8 +34,17 @@ public class GuidesPageTest extends UseCaseBase {
         boolean success = guidesPage.countrySelectorVisible();
         assertTrue(success);
     }
+
     @Test
     public void russiaPiterSelectionTest() {
         guidesPage.russiaPiterSelection();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {ACCOMPANY_GUIDE, VOLUNTEER_GUIDE})
+    public void guideTypeSelectionTest(String name) {
+        String currentGuideType = guidesPage.guideTypeSelection(name);
+        assertEquals("Accompany", currentGuideType);
+        assertEquals("Volunteer", currentGuideType);
     }
 }
