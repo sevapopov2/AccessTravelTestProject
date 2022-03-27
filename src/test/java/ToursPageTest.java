96deb7a551d62ed701b3cdafe7fce42625866f6e
Vsevolod Popov <sevapopov13@gmail.com>
2022-03-27 22:12:51 +0300

    Add tour search test.

--- src/test/java/ToursPageTest.java
+++ src/test/java/ToursPageTest.java
@@ -3,20 +3,44 @@ import Pages.ToursPage;
 import Utilities.UseCaseBase;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ToursPageTest extends UseCaseBase {
-    private MainPage mainPage;
-    private ToursPage toursPage;
+    private static MainPage mainPage;
+    private static ToursPage toursPage;
 
     @BeforeAll
-    public void classSetup() {
-        MainPage mainPage = new MainPage();
-        ToursPage toursPage = new ToursPage();
+    public static void classSetup() {
+        mainPage = new MainPage();
+        toursPage = new ToursPage();
     }
 
     @BeforeEach
     public void beforeTest() {
         mainPage.navigateToMainPage();
         mainPage.openToursPage();
+    }
+
+    @Test
+    public void countryListVisibilityTest() {
+        boolean success = toursPage.countrySelectorVisible();
+        assertTrue(success);
+    }
+
+    @Test
+    public void russiaPiterSelectionTest() {
+        toursPage.russiaPiterSelection();
+    }
+
+    @Test
+    public void tourSearchTest() {
+        toursPage.countrySelectorVisible();
+        toursPage.russiaPiterSelection();
+        toursPage.searchButtonPress();
+        toursPage.clickTourLink();
+        boolean isTourHeadingVisible = toursPage.findTourHeading();
+        assertTrue(isTourHeadingVisible);
     }
 }
